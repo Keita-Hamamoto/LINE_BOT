41acd7cd327f00253be2c8fccce53108d5a61c6c
濱本 慧太 <keita@MacBook-Air.local>
2019-05-19 20:32:05 +0900

    それなBOT

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+//APIからHerokuに渡されるリクエストをコマンドプロンプトで確認
+<?php
+// Composerでインストールしたライブラリを一括読み込み
+require_once __DIR__ . '/vendor/autoload.php';
+
+//　POSTメソッドで渡される値を取得、表示
+$inputString = file_get_contents('php://input');
+error_log($inputString);
+?>
+
+
 //じゃねーよ！！！BOT
 <?php
 //$accessToken = 'ここに「Channel Access Token」をコピペする';
