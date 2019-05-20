9caf8a7da4be4b2f6d885b36cf68e5af72c534bf
濱本 慧太 <keita@MacBook-Air.local>
2019-05-20 22:04:40 +0900

    まずはテキストを返信

--- index.php
+++ index.php
@@ -5,10 +5,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 // アクセストークンを使いCurlHTTPClientをインスタンス化
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-
 // CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
-
 // LINE Messaging APIがリクエストに付与した署名を取得
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
@@ -18,5 +16,7 @@ $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 foreach ($events as $event) {
   // テキストを返信
   $bot->replyText($event->getReplyToken(), 'TextMessage');
+}
 
 ?>
+  
