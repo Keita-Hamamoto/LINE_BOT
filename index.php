1932ce9801945318b37afaddc10b4f9345176622
濱本 慧太 <keita@MacBook-Air.local>
2019-05-21 05:55:30 +0900

    一括複数メッセージ

--- index.php
+++ index.php
@@ -38,7 +38,9 @@ foreach ($events as $event) {
   // 複数のメッセージをまとめて返信
   replyMultiMessage($bot, $event->getReplyToken(),
     //new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('TextMessage'),
-    new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('てやんでぃ！'),
+    //new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('てやんでぃ！'),
+    new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('眠いわ。'),
+    new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('雨やし暇や。'),
     new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder('https://' . $_SERVER['HTTP_HOST'] . '/imgs/original.jpg', 'https://' . $_SERVER['HTTP_HOST'] . '/imgs/preview.jpg'),
     new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder('LINE', '東京都渋谷区渋谷2-21-1 ヒカリエ27階', 35.659025, 139.703473),
     new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1, 1)
