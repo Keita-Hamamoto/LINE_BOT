7dae2332bc9053f73d6a3b17276c7bb8ee5fe93a
濱本 慧太 <keita@MacBook-Air.local>
2019-05-21 22:15:10 +0900

    一括複数メッセージ

--- index.php
+++ index.php
@@ -7,10 +7,6 @@
 // Composerでインストールしたライブラリを一括読み込み
 require_once __DIR__ . '/vendor/autoload.php';
 
-//　POSTメソッドで渡される値を取得、表示
-$inputString = file_get_contents('php://input');
-error_log($inputString);
-
 // アクセストークンを使いCurlHTTPClientをインスタンス化
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 // CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
