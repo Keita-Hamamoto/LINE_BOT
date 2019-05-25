f572e6245ebaf4e1f5b816ac87177eaf9e685378
Keita- Hamamamoto <neinneinkept@gamil.com.com>
2019-05-25 15:16:02 +0900

    saburou

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if($message_type != "text") exit;
 
 //返信メッセージ
 $return_message_text = "「" . $message_text . "」←おもろないわ";
-$return_message_text = というかちんこかゆい。;
+//$return_message_text = というかちんこかゆい。;
 
 //$return_message_text = "「" . $message_text . "」じゃねーよｗｗｗ";
 //$return_message_text = それな;
